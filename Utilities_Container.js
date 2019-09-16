9b4e06da6e94bff3cf7a90d642e2dc52c86bfad4
william <will.dowling@boombah.com>
2019-09-16 10:44:31 -0500

    added string prototype function for converting to TitleCase

--- Utilities_Container.js
+++ Utilities_Container.js
@@ -270,6 +270,11 @@ function printLog()
 	}
 }
 
+//string.toTitleCase() 
+//prototype function to convert entire string to titlecase
+String.prototype.toTitleCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
 
 //////////////
 /*
