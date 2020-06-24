7425a3445e51c9a26a380d453b85839e1e6b375c
wdjsdev <wdjsdev@gmail.com>
2020-06-24 16:00:47 -0500

    replace ampersands with quoted ampersands

--- Utilities_Container.js
+++ Utilities_Container.js
@@ -2188,7 +2188,7 @@ function curlData(url,arg)
 		scriptText += localDataFilePath;
 		
 		scriptFile = File(curlDataPath + "batFile.bat");
-		writeScriptFile(scriptFile,scriptText);
+		writeScriptFile(scriptFile,scriptText.replace("&","\"&\""));
 
 		executor = scriptFile;
 	}
