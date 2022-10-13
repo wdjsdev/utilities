dc5a86e6203a85b9a98f97197df7caf44df01421
williams <31516287+wdjsdev@users.noreply.github.com>
2022-10-13 15:19:48 -0500

    Update Utilities_Container.js
    new updates to generic recursive dig function
    custom json stringify function
    update release compound path.. still needs z-order fixes.

--- Utilities_Container.js
+++ Utilities_Container.js
@@ -669,24 +669,40 @@ function group ( items, parent )
 }
 
 
+//dig through a collection recursively and test each item
+//against the callback function. callback function returns boolean
+//if true, push the item to the array
 function recursiveDig ( item, callback )
 {
+	var resultingItems = [];
+
 	if ( item.typename.match( /document/i ) )
 	{
 		alert( "Don't dig recursively through the whole document... figure out a smarter way.." );
-		return;
+		return [];
 	}
-	if ( item.typename.match( /item/i ) )
+
+	dig( item );
+
+	return resultingItems;
+
+
+
+	function dig ( item )
 	{
-		callback( item );
-	}
-	else if ( item.typename.match( /group|layer/ ) )
-	{
-		afc( item, "pageItems" ).forEach( function ( subItem )
+		if ( item.typename.match( /group|layer/i ) )
 		{
-			recursiveDig( subItem, callback );
-		} );
+			afc( item, "pageItems" ).forEach( function ( item ) { dig( item ) } );
+		}
+		else
+		{
+			if ( callback( item ) )
+			{
+				resultingItems.push( item );
+			}
+		}
 	}
+
 }
 
 
@@ -833,6 +849,152 @@ function getUnique ( arr )
 if ( !this.JSON ) { this.JSON = {} } ( function () { function f ( n ) { return n < 10 ? "0" + n : n } if ( typeof Date.prototype.toJSON !== "function" ) { Date.prototype.toJSON = function ( key ) { return isFinite( this.valueOf() ) ? this.getUTCFullYear() + "-" + f( this.getUTCMonth() + 1 ) + "-" + f( this.getUTCDate() ) + "T" + f( this.getUTCHours() ) + ":" + f( this.getUTCMinutes() ) + ":" + f( this.getUTCSeconds() ) + "Z" : null }; String.prototype.toJSON = Number.prototype.toJSON = Boolean.prototype.toJSON = function ( key ) { return this.valueOf() } } var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, gap, indent, meta = { "\b": "\\b", "	": "\\t", "\n": "\\n", "\f": "\\f", "\r": "\\r", '"': '\\"', "\\": "\\\\" }, rep; function quote ( string ) { escapable.lastIndex = 0; return escapable.test( string ) ? '"' + string.replace( escapable, function ( a ) { var c = meta[ a ]; return typeof c === "string" ? c : "\\u" + ( "0000" + a.charCodeAt( 0 ).toString( 16 ) ).slice( -4 ) } ) + '"' : '"' + string + '"' } function str ( key, holder ) { var i, k, v, length, mind = gap, partial, value = holder[ key ]; if ( value && typeof value === "object" && typeof value.toJSON === "function" ) { value = value.toJSON( key ) } if ( typeof rep === "function" ) { value = rep.call( holder, key, value ) } switch ( typeof value ) { case "string": return quote( value ); case "number": return isFinite( value ) ? String( value ) : "null"; case "boolean": case "null": return String( value ); case "object": if ( !value ) { return "null" } gap += indent; partial = []; if ( Object.prototype.toString.apply( value ) === "[object Array]" ) { length = value.length; for ( i = 0; i < length; i += 1 ) { partial[ i ] = str( i, value ) || "null" } v = partial.length === 0 ? "[]" : gap ? "[\n" + gap + partial.join( ",\n" + gap ) + "\n" + mind + "]" : "[" + partial.join( "," ) + "]"; gap = mind; return v } if ( rep && typeof rep === "object" ) { length = rep.length; for ( i = 0; i < length; i += 1 ) { k = rep[ i ]; if ( typeof k === "string" ) { v = str( k, value ); if ( v ) { partial.push( quote( k ) + ( gap ? ": " : ":" ) + v ) } } } } else { for ( k in value ) { if ( Object.hasOwnProperty.call( value, k ) ) { v = str( k, value ); if ( v ) { partial.push( quote( k ) + ( gap ? ": " : ":" ) + v ) } } } } v = partial.length === 0 ? "{}" : gap ? "{\n" + gap + partial.join( ",\n" + gap ) + "\n" + mind + "}" : "{" + partial.join( "," ) + "}"; gap = mind; return v }return v } if ( typeof JSON.stringify !== "function" ) { JSON.stringify = function ( value, replacer, space ) { var i; gap = ""; indent = ""; if ( typeof space === "number" ) { for ( i = 0; i < space; i += 1 ) { indent += " " } } else { if ( typeof space === "string" ) { indent = space } } rep = replacer; if ( replacer && typeof replacer !== "function" && ( typeof replacer !== "object" || typeof replacer.length !== "number" ) ) { throw new Error( "JSON.stringify" ) } return str( "", { "": value } ) } } if ( typeof JSON.parse !== "function" ) { JSON.parse = function ( text, reviver ) { var j; function walk ( holder, key ) { var k, v, value = holder[ key ]; if ( value && typeof value === "object" ) { for ( k in value ) { if ( Object.hasOwnProperty.call( value, k ) ) { v = walk( value, k ); if ( v !== undefined ) { value[ k ] = v } else { delete value[ k ] } } } } return reviver.call( holder, key, value ) } text = String( text ); cx.lastIndex = 0; if ( cx.test( text ) ) { text = text.replace( cx, function ( a ) { return "\\u" + ( "0000" + a.charCodeAt( 0 ).toString( 16 ) ).slice( -4 ) } ) } if ( /^[\],:{}\s]*$/.test( text.replace( /\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, "@" ).replace( /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, "]" ).replace( /(?:^|:|,)(?:\s*\[)+/g, "" ) ) ) { j = eval( "(" + text + ")" ); return typeof reviver === "function" ? walk( { "": j }, "" ) : j } throw new SyntaxError( "JSON.parse" ) } } } )();
 
 
+//convert a json object to formatted string
+function jsonStr ( obj )
+{
+	var indentor = "  ";
+	var curDepth = 0;
+	var outputString = "";
+
+	formatObject( obj, true );
+
+	return outputString.replace( /,([\s\n\r]*[\]\}])/ig, "$1" || "," )
+
+
+	function formatObject ( obj, top )
+	{
+
+		var val; // value of the current property
+
+		outputString += "{";
+		curDepth++;
+
+		var values = [];
+		var keys = [];
+
+		for ( var key in obj )
+		{
+			if ( key === "" ) continue;
+
+			val = obj[ key ];
+			values.push( val );
+			keys.push( key );
+		}
+
+		values.forEach( function ( val, i )
+		{
+			if ( top )
+			{
+				$.writeln( "processing top level object: " + i );
+			}
+			newLine();
+			outputString += "\"" + keys[ i ] + "\": ";
+			if ( val instanceof Array )
+			{
+				formatArray( val );
+			}
+			else if ( val instanceof Object )
+			{
+				newLine();
+				formatObject( val );
+			}
+			else
+			{
+				outputString += "\"" + val + "\",";
+			}
+
+		} );
+		curDepth--;
+		newLine();
+		outputString += "}";
+		if ( curDepth > 0 )
+		{
+			outputString += ",";
+		}
+
+	}
+
+	function formatArray ( arr, index )
+	{
+		var subObjects = arr.filter( function ( v ) { return v instanceof Object } );
+
+
+		if ( subObjects.length )
+		{
+			//this is a nested array.. put the opening bracket on a new line
+			//then call formatObject on each object in the array
+			//then put the closing bracket on a new line followed by a comma
+			newLine();
+			outputString += "[";
+			curDepth++;
+			// newLine();
+			subObjects.forEach( function ( subObject, i )
+			{
+				newLine();
+				subObject instanceof Array ? formatArray( subObject ) : formatObject( subObject );
+			} )
+			curDepth--;
+			newLine();
+
+
+		}
+		else
+		{
+			//this is a flat array. put the opening bracket on the same line
+			//then put each value on the same line split by commas,
+			//then put the closing bracket on the same line followed by a comma
+			outputString += "[";
+			arr.forEach( function ( v, i, thisArr )
+			{
+				outputString += v.toString() === v ? "\"" + v + "\"" : v + ",";
+				// if ( i < thisArr.length - 1 ) outputString += ",";
+			} );
+		}
+
+		outputString += "],";
+		// if ( index < arr.length - 1 ) outputString += ",";
+	}
+
+
+
+	function newLine ()
+	{
+		outputString += "\n";
+		indent();
+	}
+
+	function indent ()
+	{
+		for ( var i = 0; i < curDepth; i++ )
+		{
+			outputString += indentor;
+		}
+	}
+}
+
+//replacer function for the JSON.stringify() method
+//this ensures that any arrays in the object are printed
+//on one line instead of each element on a new line
+function jsonArrayOnOneLineReplacerFunction ( k, v )
+{
+	if ( v instanceof Array )
+	{
+		return "[" + v.join( ", " ) + "]";
+	}
+	else if ( k === "" && !( v instanceof Object ) )
+	{
+		return undefined;
+	}
+	else
+	{
+		return v;
+	}
+}
+
+
+
+
+
 function intersects ( item, dest )
 {
 	//item coordinates
@@ -959,7 +1121,11 @@ function zoomOutToRevealAllArtwork ()
 
 	//get an array of the layers in the document
 	//and execute the dig() function on each one.
-	afc( doc, "layers" ).forEach( function ( layer ) { dig( layer ) } );
+	afc( doc, "layers" ).forEach( function ( layer ) 
+	{
+		if ( layer.locked ) return;
+		dig( layer )
+	} );
 
 	//separate the bounds into individual arrays
 	var leftBounds = bounds.map( function ( bound ) { return bound[ 0 ] } );
@@ -3224,15 +3390,47 @@ function getLibraryEntry ( lib, key )
 	}
 }
 
-function releaseCompoundPaths ( items )
+//given a compound path item, move it
+//to a sandbox layer, release the compound paths until there are zero
+//compound paths on the sandbox layer. then ungroup everything, and then
+//rebuild the compound path out of the remaining shapes. then move the
+//resulting compound path to "dest".
+function releaseCompoundPath ( item, dest )
 {
-	app.activeDocument.selection = null;
-	for ( var x = 0, len = items.length; x < len; x++ )
+	var resultingItem;
+	if ( !item.typename.match( /compoundpath/i ) ) return;
+
+	var doc = app.activeDocument;
+	var sboxLayer = doc.layers.add();
+	item.moveToBeginning( sboxLayer );
+	doc.selection = null;
+
+	var cPaths = afc( sboxLayer, "compoundPathItems" );
+	while ( cPaths.length )
 	{
-		items[ x ].selected = true;
+		doc.selection = cPaths;
+		app.executeMenuCommand( "noCompoundPath" );
+		afc( sboxLayer, "groupItems" ).forEach( function ( g ) { ungroup( g, g.parent, 0 ) } )
+		cPaths = afc( sboxLayer, "compoundPathItems" );
 	}
 
-	app.executeMenuCommand( "noCompoundPath" );
+	ungroup( sboxLayer, sboxLayer, 0 );
+
+	var sboxItems = afc( sboxLayer, "pageItems" );
+	if ( sboxItems.length === 1 )
+	{
+		resultingItem = sboxItems[ 0 ];
+	}
+	else
+	{
+		resultingItem = sboxLayer.compoundPathItems.add();
+		sboxItems.forEach( function ( item ) { item.moveToBeginning( resultingItem ); } );
+	}
+
+	resultingItem.moveToBeginning( dest );
+
+	sboxLayer.remove();
+	return resultingItem;
 }
 
 
