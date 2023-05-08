8705510a25ec1aaffc50bc5ed78d3b64cd9f7945
williams <31516287+wdjsdev@users.noreply.github.com>
2023-05-08 17:57:15 -0500

    Update Utilities_Container.js
    add align function
    also added generic export jpg function to eliminate the clunky dynamic action workaround i was using before.

--- Utilities_Container.js
+++ Utilities_Container.js
@@ -2631,6 +2631,138 @@ function hAlignRight ( key, otherObjects )
 	}
 }
 
+//customize these "alignType" arguments to your hearts desire
+//whatever tickles your brain just right.
+function align ( key, otherObjects, alignType )
+{
+	switch ( alignType )
+	{
+		//center horizontally and vertically
+		case "center":
+			doTheAligning( key, otherObjects, "vc", "hc" );
+			break;
+
+		//center vertically by centerpoints
+		case "vcenter":
+			doTheAligning( key, otherObjects, "vc", undefined );
+			break;
+
+		//align top of objects to top of key
+		case "vtop":
+			doTheAligning( key, otherObjects, "t", undefined );
+			break;
+
+		//align bottom of objects to bottom of key
+		case "vbottom":
+			doTheAligning( key, otherObjects, "b", undefined );
+			break;
+
+		//center horizontally by centerpoints
+		case "hcenter":
+			doTheAligning( key, otherObjects, undefined, "hc" );
+			break;
+
+		//align left of objects to left of key
+		case "hleft":
+			doTheAligning( key, otherObjects, undefined, "l" );
+			break;
+
+		//align right of objects to right of key
+		case "hright":
+			doTheAligning( key, otherObjects, undefined, "r" );
+			break;
+
+		//align bottom left of objects bottom left of key
+		case "botleft":
+			doTheAligning( key, otherObjects, "b", "l" );
+			break;
+
+		//align bottom right of objects bottom right of key
+		case "botright":
+			doTheAligning( key, otherObjects, "b", "r" );
+			break;
+
+		//align top left of objects top left of key
+		case "topleft":
+			doTheAligning( key, otherObjects, "t", "l" );
+			break;
+
+		//align top right of objects top right of key
+		case "topright":
+			doTheAligning( key, otherObjects, "t", "r" );
+			break;
+
+		//align left of objects to left of key, and vertical center of object to verical center of key
+		case "leftcenter":
+			doTheAligning( key, otherObjects, "vc", "l" );
+			break;
+
+		//align right of objects to right of key, and vertical center of object to verical center of key
+		case "rightcenter":
+			doTheAligning( key, otherObjects, "vc", "r" );
+			break;
+
+		//align top of objects to top of key, and horizontal center of object to horizontal center of key
+		case "topcenter":
+			doTheAligning( key, otherObjects, "t", "hc" );
+			break;
+
+		//align bottom of objects to bottom of key, and horizontal center of object to horizontal center of key
+		case "botcenter":
+			doTheAligning( key, otherObjects, "b", "hc" );
+			break;
+
+		default:
+			$.writeln( "Error: Invalid alignType argument.\nValid arguments are: center, vcenter, vtop, vbottom, hcenter, hleft, hright, botleft, botright, topleft, topright, leftcenter, rightcenter, topcenter, botcenter" );
+
+	}
+}
+
+
+function doTheAligning ( key, otherObjects, va, ha )
+{
+	var destBounds = getBoundsData( key );
+	if ( !destBounds )
+	{
+		$.writeln( "Error: No bounds data found for key object" );
+		return undefined;
+	}
+	otherObjects.forEach( function ( item )
+	{
+		var itemBounds = getBoundsData( item );
+		if ( !itemBounds.w ) return;
+		if ( va ) //vertical alignment
+		{
+			switch ( va )
+			{
+				case "vc":
+					item.top = destBounds.vc + itemBounds.hh; //vertical center point of key + half height of item
+					break;
+				case "t":
+					item.top = destBounds.t; //top of key
+					break;
+				case "b":
+					item.top = destBounds.b + itemBounds.h; //bottom of key + height of item
+			}
+
+		}
+		if ( ha ) //horizontal alignment
+		{
+			switch ( ha )
+			{
+				case "hc":
+					item.left = destBounds.hc - itemBounds.hw; //horizontal center point of key - half width of item
+					break;
+				case "l":
+					item.left = destBounds.l; //left of key
+					break;
+				case "r":
+					item.left = destBounds.r - itemBounds.w; //right of key - width of item
+			}
+		}
+	} );
+}
+
 
 
 //send customized emails
@@ -4066,6 +4198,20 @@ function rotatePieces ( rotationSets, parentLayer )
 
 
 }
+
+
+function exportJpg ( outFilePath, bounds, resolution )
+{
+	var clipBounds = bounds;
+	var imgCapOptions = new ImageCaptureOptions();
+	imgCapOptions.resolution = resolution;
+	imgCapOptions.antiAliasing = true;
+
+	app.activeDocument.imageCapture( File( outFilePath ), clipBounds, imgCapOptions );
+
+}
+
+
 
 
 //
